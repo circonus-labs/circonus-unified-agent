aa590e3eb653b1175cae3e9c1ddf37584d6c8886
maier <maier@users.noreply.github.com>
2021-01-28 15:11:23 -0500

    add: health output to all

--- plugins/outputs/all/all.go
+++ plugins/outputs/all/all.go
@@ -4,4 +4,5 @@ import (
 	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/circonus"
 	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/discard"
 	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/file"
+	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/health"
 )

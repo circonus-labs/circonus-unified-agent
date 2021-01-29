5f2758a3388526f591897005deb017f786796b3e
maier <maier@users.noreply.github.com>
2021-01-29 10:44:26 -0500

    upd: cleanup commented code

--- plugins/outputs/circonus/circonus.go
+++ plugins/outputs/circonus/circonus.go
@@ -165,14 +165,12 @@ func (c *Circonus) Write(metrics []cua.Metric) error {
 				if strings.Contains(s, "H[") && strings.Contains(s, "]=") {
 					numMetrics += c.buildHistogram(defaultDest, m)
 				} else {
-					// c.Log.Debugf("Metric type is %v", m.Type())
 					numMetrics += c.buildTexts(defaultDest, m)
 				}
 			} else {
 				numMetrics += c.buildNumerics(defaultDest, m)
 			}
 		case cua.Histogram:
-			// c.Log.Debugf("Metric type is histogram", m.Type())
 			numMetrics += c.buildHistogram(defaultDest, m)
 		case cua.CumulativeHistogram:
 			numMetrics += c.buildCumulativeHistogram(defaultDest, m)

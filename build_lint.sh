bbfe2515c9ea77746faf27d4920a171548ee58ce
maier <maier@users.noreply.github.com>
2021-02-24 13:14:26 -0500

    upd: build/lint configs

---
+++ build_lint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo
+echo "Running Lint (Linux specific)"
+GOOS=linux golangci-lint run || exit 1
+echo "Running Lint (Windows specific)"
+GOOS=windows golangci-lint run || exit 1
+

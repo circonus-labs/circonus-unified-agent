6afc4203bd2957da173fecb32efb8b10e21d54c3
maier <maier@users.noreply.github.com>
2021-05-19 11:00:51 -0400

    add: remaning metric prefix includes

--- plugins/inputs/stackdriver_circonus/stackdriver.go
+++ plugins/inputs/stackdriver_circonus/stackdriver.go
@@ -30,7 +30,7 @@ const (
 	sampleConfig     = `
   # Instance ID is required
   instance_id = ""
-  
+
   ## GCP Project
   project = "erudite-bloom-151019"
 
@@ -778,15 +778,63 @@ func init() {
 			RateLimit: defaultRateLimit,
 			Delay:     defaultDelay,
 			MetricTypePrefixInclude: []string{
-				"compute.googleapis.com/",
-				"logging.googleapis.com/",
-				"bigquery.googleapis.com/",
-				"dns.googleapis.com/",
-				"loadbalancing.googleapis.com/",
-				"networking.googleapis.com/",
-				"storage.googleapis.com/",
-				"firebasehosting.googleapis.com/",
-				"redis.googleapis.com/",
+				"actions.googleapis.com/",              // Google Assistant Smart Home
+				"aiplatform.googleapis.com/",           // AI Platform (Unified)
+				"apigateway.googleapis.com/",           // API Gateway
+				"apigee.googleapis.com/",               // Apigee
+				"appengine.googleapis.com/",            // App Engine
+				"autoscaler.googleapis.com/",           // Compute Engine Autoscaler
+				"bigquery.googleapis.com/",             // BigQuery
+				"bigquerybiengine.googleapis.com/",     // BigQuery BI Engine
+				"bigquerydatatransfer.googleapis.com/", // BigQuery Data Transfer Service
+				"bigtable.googleapis.com/",             // Cloud BigTable
+				"cloudfunctions.googleapis.com/",       // Cloud Functions
+				"cloudiot.googleapis.com/",             // IoT Core
+				"cloudsql.googleapis.com/",             // Cloud SQL
+				"cloudtasks.googleapis.com/",           // Cloud Tasks (formerly App Engine Task Queue)
+				"cloudtrace.googleapis.com/",           // Cloud Trace
+				"composer.googleapis.com/",             // Cloud Composer
+				"compute.googleapis.com/",              // Compute Engine
+				"dataflow.googleapis.com/",             // Dataflow
+				"dataproc.googleapis.com/",             // Dataproc
+				"datastore.googleapis.com/",            // Datastore
+				"dlp.googleapis.com/",                  // Cloud Data Loss Prevention
+				"dns.googleapis.com/",                  // Cloud DNS
+				"file.googleapis.com/",                 // Filestore
+				"firebaseappcheck.googleapis.com/",     // Firebase (1 metric: verdicts from performing Firebase App Check verifications)
+				"firebasedatabase.googleapis.com/",     // Firebase (database metrics)
+				"firebasehosting.googleapis.com/",      // Firebase (general hosting)
+				"firebasestorage.googleapis.com/",      // Cloud Storage for Firebase
+				"firestore.googleapis.com/",            // Firestore
+				"firewallinsights.googleapis.com/",     // Firewall Insights
+				"healthcare.googleapis.com/",           // Cloud Healthcare API
+				"iam.googleapis.com/",                  // Identity & Access Management
+				"interconnect.googleapis.com/",         // Cloud Interconnect
+				"loadbalancing.googleapis.com/",        // Cloud Load Balancing
+				"logging.googleapis.com/",              // Cloud Logging
+				"managedidentities.googleapis.com/",    // Managed Service for Microsoft Active Directory
+				"memcache.googleapis.com/",             // Memorystore for Memcached
+				"metastore.googleapis.com/",            // Dataproc Metastore
+				"ml.googleapis.com/",                   // AI Platform (formerly Cloud Machine Learning)
+				"monitoring.googleapis.com/",           // Cloud Monitoring
+				"networking.googleapis.com/",           // Network Topology
+				"networksecurity.googleapis.com/",      // Google Cloud Armor
+				"privateca.googleapis.com/",            // Certificate Authority Service
+				"pubsub.googleapis.com/",               // Pub/Sub
+				"pubsublite.googleapis.com/",           // Pub/Sub Lite
+				"recaptchaenterprise.googleapis.com/",  // reCAPTCHA Enterprise
+				"recommendationengine.googleapis.com/", // Recommendations AI
+				"redis.googleapis.com/",                // Memorystore for Redis
+				"router.googleapis.com/",               // Cloud Router
+				"run.googleapis.com/",                  // Cloud Run
+				"serviceruntime.googleapis.com/",       // all Google Cloud APIs & Cloud Endpoints
+				"spanner.googleapis.com/",              // Cloud Spanner
+				"storage.googleapis.com/",              // Cloud Storage
+				"storagetransfer.googleapis.com/",      // Storage Transfer Service for on-premises data
+				"tpu.googleapis.com/",                  // Cloud TPU
+				"vpcaccess.googleapis.com/",            // Virtual Private Cloud (VPC)
+				"vpn.googleapis.com/",                  // Cloud VPN
+				"workflows.googleapis.com/",            // Workflows
 			},
 			MetricTypePrefixExclude:         []string{},
 			GatherRawDistributionBuckets:    true,

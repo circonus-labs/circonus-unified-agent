9fbbd9240764c5890619630007568fc514c0532d
maier <maier@users.noreply.github.com>
2022-09-08 09:04:40 -0400

    CIRC-9029 feat: add elasticsearch output plugin

--- plugins/outputs/all/all.go
+++ plugins/outputs/all/all.go
@@ -4,6 +4,7 @@ package all
 import (
 	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/circonus"
 	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/discard"
+	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/elasticsearch"
 	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/file"
 	_ "github.com/circonus-labs/circonus-unified-agent/plugins/outputs/health"
 )
